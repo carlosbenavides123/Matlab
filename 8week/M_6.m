2922015716254b67859904e6e945e518ec1a6c95
carlos <carlos7legend@gmail.com>
2018-10-25 10:51:58 -0700

    wip

--- 8week/M_6.m
+++ 8week/M_6.m
@@ -12,22 +12,56 @@ syms t n aSym(n) bSym(n) x(t) f(t)
 %This appears to b needed to work with the symbolic toolbox 
 %for this application
 
-%This is a specifice example for 6.1-b
-tStart = -pi;
-tStop = pi;
-To = 10*pi;
-f(t) = 1;
-wo = 2*pi/To;
-x(t) = f(t) *(heaviside(t-tStart)-heaviside(t-tStop));
+% for 6.1a
+% tStart = -1;
+% tStop = 1;
+% To = 4;
+% f(t) = 2;
+% wo = 2*pi/To;
+% x(t) = f(t) * (heaviside(t-tStart)-heaviside(t-tStop));
+% 
+% 
+% %This is a specifice example for 6.1-b
+% tStart = -pi;
+% tStop = pi;
+% To = 10*pi;
+% f(t) = 1;
+% wo = 2*pi/To;
+% x(t) = f(t) * (heaviside(t-tStart)-heaviside(t-tStop));
+% 
+% % For 6.1-c
+% tStart = 0;
+% tStop = 2*pi;
+% To = 2*pi;
+% f(t) = 1;
+% wo = 2*pi/To;
+% x(t) = f(t) * (heaviside(t-tStart)-heaviside(t-tStop));
+% 
+% % for 6.1-d
+% tStart = -pi/4;
+% tStop = pi/4;
+% To = pi;
+% f(t) = 1;
+% wo = 2*pi/To;
+% x(t) = f(t) * (heaviside(t-tStart)-heaviside(t-tStop));
 
-a0 = int(x(t),t,tStart,tStop)/To
+% for 6.1-e
+tStart = 0;
+tStop = 1;
+To = t;
+f(t) = 4;
+wo = 2*pi/To;
+x(t) = f(t) * (heaviside(t-tStart)-heaviside(t-tStop));
+
+
+a0       = int(x(t),t,tStart,tStop)/To
 aSym(n)  = simplify(int(x(t)*cos(n*wo*t),t,tStart,tStop)/(To/2))
 bSym(n)  = simplify(int(x(t)*sin(n*wo*t),t,tStart,tStop)/(To/2))
 
-nMax = 20;
-n = 1:nMax;
-a = eval(aSym(n));
-b = eval(bSym(n));
+nMax    = 20;
+n       = 1:nMax;
+a       = eval(aSym(n));
+b       = eval(bSym(n));
 
 figure
 subplot(3,1,1)
