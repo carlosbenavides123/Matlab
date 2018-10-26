4e765bf639104ef11e1dcb11cfce9ebed0d7d50a
carlos <carlos7legend@gmail.com>
2018-10-25 20:06:14 -0700

    fix b

--- 8week/b.m
+++ 8week/b.m
@@ -1,47 +1,51 @@
 %file name: M_6.m
-%Last modified by Carlos Benavides on 10/24/2018
+%Last modified by David Schwartz on 10/11/2018
 clear all
 format compact
 clc
 
 %define the symbolic variables that we will need
-syms t n aSym(n) bSym(n) f(t)
-
+syms t n aSym(n) bSym(n) x(t) f(t)
 
 %Using the heaviside built-in function instead of an in-line u(t)
-%This appears to b needed to work with the symbolic toolbox 
+%This appears to b needed to work with the symbolic toolbox
 %for this application
 
-for 6.1-e
-tStart = 0;
-tStop = 1;
-To = t;
-f(t) = 4;
-wo = 2*pi/To;
-x(t) = f(t) * (heaviside(t-tStart)-heaviside(t-tStop));
+%This is a specifice example for 6.1-b
+tStart = -pi;
+tStop = pi;
+To = 10 * pi;
+f(t) = 1;
+wo = 2 * pi / To;
+x(t) = f(t) * (heaviside(t - tStart) - heaviside(t - tStop));
 
-nMax    = 20;
-n       = 1:nMax;
-a       = eval(aSym(n)+aSym2(n));
-b       = eval(bSym(n)+bSym2(n));
+a0 = int(x(t), t, tStart, tStop) / To
+aSym(n) = simplify(int(x(t) * cos(n * wo * t), t, tStart, tStop) / (To / 2))
+bSym(n) = simplify(int(x(t) * sin(n * wo * t), t, tStart, tStop) / (To / 2))
+
+nMax = 20;
+n = 1:nMax;
+a = eval(aSym(n));
+b = eval(bSym(n));
 
 figure
-subplot(3,1,1)
-stem(n,a)
+subplot(3, 1, 1)
+stem(n, a)
 ylabel('a(n)');
-subplot(3,1,2);
-stem(n,b)
+subplot(3, 1, 2);
+stem(n, b)
 ylabel('b(n)');
 
-xApprox = a0+a20;
+xApprox = a0;
+
 for m = 1:nMax
-    xApprox = xApprox + a(m)*cos(m*wo*t) + b(m)*sin(m*wo*t);
+    xApprox = xApprox + a(m) * cos(m * wo * t) + b(m) * sin(m * wo * t);
 end
-t = -20*pi:0.1:20*pi;
+
+t = -20 * pi:0.1:20 * pi;
 
 xCalc = eval(xApprox);
-subplot(3,1,3);
-plot(t,xCalc)
+subplot(3, 1, 3);
+plot(t, xCalc)
 grid on
 ylabel('x(t)')
-xlim([-7 7])
