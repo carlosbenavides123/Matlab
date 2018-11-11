534e498eaf08cf2533bc37ac3c35f6d01fa5139a
carlos <carlos7legend@gmail.com>
2018-11-11 12:24:38 -0800

    able to step through alpha values and create MULTIPLE figures

--- 7matlab/matlab_assignment_7_fourier_transform.m
+++ 7matlab/matlab_assignment_7_fourier_transform.m
@@ -7,48 +7,68 @@ format compact
 clc 
 
 % define symbolics
-syms w t x(t) X(w) T W; % be careful performance issue.
+% this test value is used to be the index for 
+% xxApprox(test,:) = eval(xApprox(t))
+% I noticed that xxAprox was creating 1 row and like 200 columns
+% so I use test to create a matrix
+test = 1
 
-% write comment
-x(t) = heaviside(t+T/2) - heaviside(t-T/2);
-X(w) = simplify(fourier(x(t),w));
-alpha = 0.90;
-T = 2;
-X(w) = eval(X(w));
-Xsq(w) = X(w)*conj(X(w));
-Etot = int(Xsq(w) ,- inf, inf) / ( 2 * pi );
-E(W) = int(Xsq(w), -W, W) / (2 * pi);
+% step through everything with respect to i
+% i is suppose to mimic alpha
+% we will be creating a lot of figures...
+for i = 0.70:0.02:0.98
 
-maxW = 10;
+    syms w t x(t) X(w) T W; % be careful performance issue.
 
-% plot
-subplot(3,1,1)
-fplot(Xsq(w),[0 maxW])
-grid on
-ylabel('|X(w)|^2');
-xlabel('w ');
+    x(t) = heaviside(t+T/2) - heaviside(t-T/2);
+    X(w) = simplify(fourier(x(t),w));
+    T = 2;
+    
+    X(w) = eval(X(w));
+    Xsq(w) = X(w)*conj(X(w));
+    Etot = int(Xsq(w) ,- inf, inf) / ( 2 * pi );
+    E(W) = int(Xsq(w), -W, W) / (2 * pi);
 
-subplot(3,1,2)
-fplot(3,1,2)
-fplot(E(W)/Etot, [0 maxW])
-grid on
-ylabel('E(w)')
-xlabel('W (radians/sec)')
-hold on
+    maxW = 10;
 
-Web = vpa(vpasolve(E(W) == alpha*Etot),3);
-plot([0 maxW], [alpha alpha], '--')
-plot([Web Web], [0 1], '--')
-hold off
-subplot(3,1,3)
+    % plot
+    figure(test)
+    subplot(3,1,1)
+    fplot(Xsq(w),[0 maxW])
+    grid on
+    ylabel('|X(w)|^2');
+    xlabel('w ');
 
-xApprox(t) = (1/(2*pi))*int(X(w)*exp(j*w*t),w,-Web,Web);
-t=-10:0.1:10;
+    subplot(3,1,2)
+    fplot(E(W)/Etot, [0 maxW])
+    grid on
 
-xxApprox = eval(xApprox(t));
-plot(t,xxApprox)
-grid on
-hold on
-xx = eval(x(t));
-plot(t,xx);
-hold off;
+    ylabel('E(w)')
+    xlabel('W (radians/sec)')
+    hold on
+
+    Web = vpa(vpasolve(E(W) == i*Etot),3);
+    plot([0 maxW], [i i], '--')
+    plot([Web Web], [0 1], '--')
+    hold off
+    subplot(3,1,3)
+    
+    xApprox(t) = (1/(2*pi))*int(X(w)*exp(j*w*t),w,-Web,Web)
+    t=-10:0.1:10;
+
+    xxApprox(test,:) = eval(xApprox(t))
+    plot(t,xxApprox(test,:))
+    grid on
+    hold on
+    xx = eval(x(t));
+    plot(t,xx);
+    txt = ['The alpha valus is ',num2str(i)];
+    ylabel(txt)
+    hold off;
+
+    % increment test to add xxAprox to next row in the matrix
+    test = test + 1
+
+    % reset every symbolic to avoid issues
+    reset(symengine)
+end
